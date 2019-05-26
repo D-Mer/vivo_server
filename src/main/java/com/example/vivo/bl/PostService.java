9387636763ca0554a7ba91c7301b84627f55f606
JunboXu <43670418+JunboXu@users.noreply.github.com>
2019-05-26 17:48:43 +0800

    完成后端post部分
    getposts；getpostbyid；addPost；takeOrder；giveUp；complete；

--- src/main/java/com/example/vivo/bl/PostService.java
+++ src/main/java/com/example/vivo/bl/PostService.java
@@ -1,4 +1,26 @@
 package com.example.vivo.bl;
 
+import com.example.vivo.vo.OrderForm;
+import com.example.vivo.vo.PostForm;
+import com.example.vivo.vo.ResponseVO;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.sound.sampled.ReverbType;
+
 public interface PostService {
+
+    /*
+    * @param PostForm
+    * @return
+    */
+    ResponseVO showPosts(String email);
+
+
+    ResponseVO addPosts(PostForm postForm);
+
+    ResponseVO takeOrder(OrderForm orderForm);
+
+    ResponseVO giveUp(OrderForm orderForm);
+
+    ResponseVO completePost(String postId);
 }
---
+++ src/main/java/com/example/vivo/vo/OrderForm.java
@@ -0,0 +1,9 @@
+package com.example.vivo.vo;
+
+import lombok.Data;
+
+@Data
+public class OrderForm {
+    private int id;
+    private String takerEmail;
+}
